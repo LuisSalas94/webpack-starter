2a2c5c0bb339fb371af3e1b41d4e5eca6de2f2d6
Luis <luisfernandosalasg@gmail.com>
2020-04-12 00:39:32 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import{saludar} from './js/componentes.js';
+import './styles.css';
+
+
+
+const nombre = 'Luis';
+saludar(nombre);
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin       = require('html-webpack-plugin');
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const Copyplugin              = require('copy-webpack-plugin');
+ 
+ 
+module.exports = {
+ 
+    mode: 'development',
+    optimization:{
+        minimizer:[new OptimizeCssAssetsPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]   
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ] 
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize:false,
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use :[
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            esModule:false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'[name].css',
+            ignoreOrder: false
+        }),
+
+        new Copyplugin([
+            {from : 'src/assets', to: 'assets/'}
+        ])
+    ]
+ 
+    
+}
